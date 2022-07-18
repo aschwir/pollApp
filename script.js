599e8463566dbfd93f1cd020935c73d3ef9fd091
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-17 21:35:30 -0400

    started code

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const poll = {
+  question: "What is your favorite programming language?",
+  options: ["0: Javascript", "1: Python", "2: Rust,", "3: C++"],
+  anwsers: new Array(4).fill(0),
+};
