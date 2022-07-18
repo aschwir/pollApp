3b99837cbc6429920feac3af412dad943726a8b3
andrew schwirzinski <aschwirzinski@gmail.com>
2022-07-17 21:49:40 -0400

    prompt user for anwser

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ const poll = {
   question: "What is your favorite programming language?",
   options: ["0: Javascript", "1: Python", "2: Rust,", "3: C++"],
   anwsers: new Array(4).fill(0),
+
+  registerNewAnwser() {
+    //Get the anwser
+    const anwser = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+    console.log(anwser);
+
+    //Register the anwser
+  },
 };
+
+poll.registerNewAnwser();
